5e95a26c91e45b1fc88c31f57d86767711930e51
Steve <100400460+Inkkz@users.noreply.github.com>
2022-08-16 12:55:46 +0300

    Update Command.ts

--- src/Types/Command.ts
+++ src/Types/Command.ts
@@ -32,4 +32,4 @@ interface ICommandConfig {
     cooldown?: number
 }
 
-export type TCategory = 'dev' | 'general' | 'weeb' | 'utils' | 'fun' | 'moderation' | 'media' | 'category' | 'nsfw'
+export type TCategory = 'dev' | 'coding' | 'nature' | 'educative' | 'general' | 'weeb' | 'utils' | 'fun' | 'moderation' | 'media' | 'category' | 'nsfw'
